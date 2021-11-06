2d1598a87ebcb51e71ed33dd5d8d79e626a250bc
Duc Nguyen <minhduc140583@gmail.com>
2021-11-06 11:21:09 +0700

    Refactor LoadProject

--- generator/extension.go
+++ generator/extension.go
@@ -53,16 +53,34 @@ func GenerateFromString(projectName, jsonInput string, initEnv func(map[string]s
 	input.Env = initEnv(input.Env, projectName)
 	return nil
 }
-func DecodeProject(byteValue []byte, projectName string, initEnv func(map[string]string, string) map[string]string) (metadata.Project, error) {
+func DecodeProject(byteValue []byte, projectName string, initEnv func(map[string]string, string) map[string]string, models...[]metadata.Model) (metadata.Project, error) {
 	var input metadata.Project
 	err := json.NewDecoder(bytes.NewBuffer(byteValue)).Decode(&input)
 	if err != nil {
 		return input, err
 	}
-	input.Env = initEnv(input.Env, projectName)
+	if initEnv != nil {
+		input.Env = initEnv(input.Env, projectName)
+	}
+	if len(models) > 0 && models[0] != nil {
+		input.Models = models[0]
+	}
 	return input, err
 }
-func ExportProject(project metadata.Project, models []metadata.Model) metadata.Project {
-	project.Models = models
-	return project
+func LoadProject(projectTemplateName, projectName string, templates map[string]string, m []metadata.Model, initEnv func(map[string]string, string) map[string]string) (*metadata.Project, error) {
+	if data, ok := templates[projectTemplateName]; ok {
+		pr, err := DecodeProject([]byte(data), projectName, initEnv, m)
+		if err != nil {
+			return nil, err
+		}
+		return &pr, nil
+	}
+	return nil, nil
+}
+func ExportProject(container string, load func(string) (map[string]string, error), projectTemplateName, projectName string, m []metadata.Model, initEnv func(map[string]string, string) map[string]string) (*metadata.Project, error) {
+	templates, err := load(container)
+	if err != nil {
+		return nil, err
+	}
+	return LoadProject(projectTemplateName, projectName, templates, m, initEnv)
 }
