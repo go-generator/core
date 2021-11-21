dd691f0a3cdc0737a7efd0ab9b0d8887299b76e8
Nhut Thanh <nhutthanh1096@gmail.com>
2021-11-21 20:23:00 +0700

    update detect

--- export/db/detect.go
+++ export/db/detect.go
@@ -1,8 +1,18 @@
 package db
 
 import (
+	"errors"
 	"path/filepath"
 	"strings"
+)
+
+const (
+	DriverPostgres   = "postgres"
+	DriverMysql      = "mysql"
+	DriverMssql      = "mssql"
+	DriverOracle     = "oracle"
+	DriverSqlite3    = "sqlite3"
+	DriverNotSupport = "no support"
 )
 
 func DetectDriver(s string) string {
@@ -25,5 +35,35 @@ func DetectDriver(s string) string {
 				return "sqlite3"
 			}
 		}
+	}
+}
+
+func ExtractDBName(dsn string, driver string) (string, error) {
+	switch driver {
+	case DriverMysql:
+		s1 := strings.Split(dsn, "/")
+		if len(s1) < 2 {
+			return "", errors.New("invalid datasource")
+		}
+		s2 := strings.Split(s1[1], "?")
+		return s2[0], nil
+	case DriverPostgres:
+		s1 := strings.Split(dsn, "dbname=")
+		if len(s1) < 2 {
+			return "", errors.New("invalid datasource")
+		}
+		s2 := strings.Split(s1[1], " ")
+		return s2[0], nil
+	case DriverMssql:
+		s1 := strings.Split(dsn, "database=")
+		if len(s1) < 2 {
+			return "", errors.New("invalid datasource")
+		}
+		s2 := strings.Split(s1[1], "&")
+		return s2[0], nil
+	case DriverSqlite3:
+		return filepath.Base(dsn), nil
+	default:
+		return "", errors.New(DriverNotSupport)
 	}
 }
