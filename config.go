3cc20e546119bd9bb30fa8dc3eb4ed0102e4356f
Duc Nguyen <minhduc140583@gmail.com>
2021-11-21 22:10:21 +0700

    Add prefix in config

--- config.go
+++ config.go
@@ -1,6 +1,8 @@
 package metadata
 
 type Config struct {
+	Prefix      string `yaml:"prefix" mapstructure:"prefix" json:"prefix,omitempty" gorm:"column:prefix" bson:"prefix,omitempty" dynamodbav:"prefix,omitempty" firestore:"prefix,omitempty"`
+	Suffix      string `yaml:"suffix" mapstructure:"suffix" json:"suffix,omitempty" gorm:"column:suffix" bson:"suffix,omitempty" dynamodbav:"suffix,omitempty" firestore:"suffix,omitempty"`
 	Project     string `yaml:"project" mapstructure:"project" json:"project,omitempty" gorm:"column:project" bson:"project,omitempty" dynamodbav:"project,omitempty" firestore:"project,omitempty"`
 	ProjectName string `yaml:"project_name" mapstructure:"project_name" json:"projectName,omitempty" gorm:"column:projectname" bson:"projectName,omitempty" dynamodbav:"projectName,omitempty" firestore:"projectName,omitempty"`
 	ProjectPath string `yaml:"project_path" mapstructure:"project_path" json:"projectPath,omitempty" gorm:"column:projectpath" bson:"projectPath,omitempty" dynamodbav:"projectPath,omitempty" firestore:"projectPath,omitempty"`
